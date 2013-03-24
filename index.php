7fd127bdb048be619e00d7f407d645c9bd5bc59d
Cyandelta <cyandelta@hotmail.com>
2013-03-24 18:54:05 +0000

    Created index.php and added license info

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/*******************************************************************************
+*  Title: CGuestbook 1.0
+*  Version: 1.0 from 2013
+*  Author: Cyandelta
+********************************************************************************
+*  COPYRIGHT NOTICE
+*  Copyright 2009-2013 Cyandelta. All Rights Reserved.
+
+*  The CGuestbook may be used and modified free of charge by anyone
+*  AS LONG AS COPYRIGHT NOTICES AND ALL THE COMMENTS REMAIN INTACT.
+*  By using this code you agree to indemnify Cyandelta from any
+*  liability that might arise from it's use.
+
+*  Selling the code for this program, in part or full, without prior
+*  written consent is expressly forbidden.
+
+*  Using this code, in part or full, to create derivate work,
+*  new scripts or products is expressly forbidden. Obtain permission
+*  before redistributing this software over the Internet or in
+*  any other medium. In all cases copyright and header must remain intact.
+*  This Copyright is in full effect in any country that has International
+*  Trade Agreements with the United States of America or
+*  with the European Union.
+*******************************************************************************/
